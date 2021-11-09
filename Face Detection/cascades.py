75b647dbf7caab1dfaa705afb26f912028637cf8
kamath7 <adithyakamath96@gmail.com>
2021-11-09 23:43:47 +0530

    starting with detection of faces

--- Face Detection/cascades.py
+++ Face Detection/cascades.py
@@ -1,8 +1,15 @@
 import cv2
 
-#loading some cascades. Already downloaded
+# loading some cascades. Already downloaded
 
-#cascades for the eyes 
+# cascades for the eyes
 face_casca = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
-#cascades for the face
+# cascades for the face
 eye_casca = cv2.CascadeClassifier('.//haarcascade_eye.xml')
+
+# func to detect faces
+
+
+def detect_my_face(gray, frame):
+    # getting coordinates of the face
+    faces = face_casca.detectMultiScale(gray, 1.3, 5)
