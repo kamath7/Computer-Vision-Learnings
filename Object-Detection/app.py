5462fec8fb559feb39037bc3b2f8cf17e4e3d653
kamath7 <adithyakamath96@gmail.com>
2021-11-11 21:30:09 +0530

    starting with detection of things

--- Object-Detection/app.py
+++ Object-Detection/app.py
@@ -5,3 +5,8 @@ from data import BaseTransform, VOC_CLASSES as labelmap
 from ssd import build_ssd
 import imageio
 
+#creating the function 
+
+def detectThings(frame, net, transform): #neural net here is our SSD, transform to make it comptaible with the NN
+    height, width = frame.shape[:2] #height and width of the frame. shape is an in-built func 
+    
