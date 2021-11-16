6b895c9741822e1afd117b98b0f6aba891245579
kamath7 <adithyakamath96@gmail.com>
2021-11-17 01:14:16 +0530

    defining the neural net for the Generator

--- GAN/app.py
+++ GAN/app.py
@@ -31,3 +31,20 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         m.weightdata.normal_(1.0, 0.2)
         m.bias.data.fill_(0)
+
+# defining the generator class. Inherited nn.module to create neural net
+
+
+class G(nn.Module):
+
+    def __init__(self):
+        super(G, self).__init__()
+        # meta module for all different layers/methods. Inverse CNN is created here
+        self.main = nn.Sequential(nn.ConvTranspose2d(100, 512, 4, 1, 0, bias=False), nn.BatchNorm2d(
+            512), nn.ReLU(True), nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(
+            256), nn.ReLU
+            (True), nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False), nn.BatchNorm2d(
+            128), nn.ReLU(True),  nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),  nn.BatchNorm2d(
+            64), nn.ReLU(True), nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False), nn.Tanh()
+        )  # defining layers of the nn. 3 channels
