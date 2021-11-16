7d13e2ff1508a537f164576068a238d68e0c7082
kamath7 <adithyakamath96@gmail.com>
2021-11-17 00:22:27 +0530

    defining parameters and starting with transformations and data reading

--- GAN/app.py
+++ GAN/app.py
@@ -10,3 +10,13 @@ import torchvision.utils as vutils
 from torch.autograd import Variable
 
 
+#parameters 
+batchSize = 69 
+imageSize = 69
+
+transform = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)),]) #transforming ip images
+
+#reading dataset . we use CIFAR for training
+
+dataset = dset.CIFAR10(root='./data', download=True, transform=transform)
+dataloader = torch.utils.data.dataloader(dataset, batch_size = batchSize, shuffle=True, num_workers=2)
